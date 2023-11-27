594f23a97db2ce387f507b22509b5b9fa17bfc76
sabumi2002 <57219910+sabumi2002@users.noreply.github.com>
2023-11-27 10:39:39 +0900

    Delete user-service directory

--- user-service/src/main/java/com/example/userservice/jpa/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.userservice.jpa;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
-
-}
--- user-service/src/main/java/com/example/userservice/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.userservice.service;
-
-import com.example.userservice.dto.UserDto;
-
-public interface UserService {
-    UserDto createUser(UserDto userDto);
-}
