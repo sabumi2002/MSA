1e6fed7d0aa983e3dd16ac5872d46350173f4c82
DESKTOP-CMCIV5V\BIT <sabumi2002@naver.com>
2023-11-27 17:59:55 +0900

    catalog-service 사용자조회

---
+++ catalog-service/src/main/java/com/example/catalogservice/dto/CatalogDto.java
@@ -0,0 +1,11 @@
+package com.example.catalogservice.dto;
+
+public class CatalogDto {
+    private String productId;
+    private Integer qty;
+    private Integer unitPrice;
+    private Integer totalPrice;
+
+    private String orderId;
+    private String userId;
+}
---
+++ samp_project_market/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -0,0 +1,65 @@
+package com.example.userservice.service;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.jpa.UserEntity;
+import com.example.userservice.jpa.UserRepository;
+import com.example.userservice.vo.ResponseOrder;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService{
+    @Autowired
+    UserRepository userRepository;
+    BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        userDto.setUserId(UUID.randomUUID().toString());
+
+        // dto -> entity 객체로 변환 시키기 위한 객체
+        ModelMapper mapper = new ModelMapper();
+        // modelmapper가 변환할 수 있는 환경설정 설정정보 지정 (strategy는 딱 맞아 떨어지지않으면 지정이 안됨.)
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        // 변환 작업
+        UserEntity userEntity = mapper.map(userDto, UserEntity.class);
+        userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
+
+        userRepository.save(userEntity);
+        UserDto returnUserDto = mapper.map(userEntity, UserDto.class);
+
+        return returnUserDto;
+    }
+    @Override
+    public UserDto getUserByUserId(String userId) {
+        UserEntity userEntity = userRepository.findByUserId(userId);
+
+        if(userEntity == null)
+            throw new UsernameNotFoundException("User not found");
+
+        UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
+
+        List<ResponseOrder> orders = new ArrayList<>();
+        userDto.setOrders(orders);
+
+        return userDto;
+    }
+
+    @Override
+    public Iterable<UserEntity> getUserByAll() {
+        return userRepository.findAll();
+    }
+}
