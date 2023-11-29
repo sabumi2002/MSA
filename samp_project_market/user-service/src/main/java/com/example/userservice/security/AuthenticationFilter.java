00465e205691799884ce3ca5c0c07179bf48b7b6
DESKTOP-CMCIV5V\BIT <sabumi2002@naver.com>
2023-11-30 08:55:30 +0900

    security ip 제한

---
+++ samp_project_market/user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
@@ -0,0 +1,51 @@
+package com.example.userservice.security;
+
+import com.example.userservice.vo.RequestLogin;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+
+    @Override
+    // 요청정보를 보냈을때 그것을 처리해줄수있는 메소드 재정의
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+        try {
+            // request.getInputStream() : 전달시켜주려는 로그인값이 post형식으로 전달된다. post형식을 requestParameter로 받을수 없기때문에 inputstream으로 수작업하도록한다.
+            RequestLogin creds = new ObjectMapper().readValue(request.getInputStream(), RequestLogin.class);
+
+            // getAuthenticationManager().authenticate(): 인증작업 요청
+            // 사용자가 입력했던 email(ID), password 값을 스프링시큐리티에서 사용할수있는 형태의값으로 변환하기위해선 UsernamePasswordAuthenticationToken 으로 변경할 필요가 있다.
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getEmail(),       // ID
+                            creds.getPassword(),    // PW
+                            new ArrayList<>()       // 어떤 권한을 가질것인지
+                    )
+            );
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    // 실제 로그인 성공했을때 정확히 어떤 처리를 해줄것인지 정의
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+}
--- samp_project_market/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ samp_project_market/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -3,60 +3,50 @@ package com.example.userservice.security;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import java.util.function.Supplier;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
+    private static final String IP_ADDRESS = "59.10.164.35";
+    private static final String SUBNET = "/32";
+    private static final IpAddressMatcher IP_ADDRESS_MATCHER = new IpAddressMatcher(IP_ADDRESS + SUBNET);
     private static final String[] WHITE_LIST = {
             "/**",
             "/users/**"
     };
 
-    //    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(csrf -> csrf
-//                        .ignoringRequestMatchers(PathRequest.toH2Console())
-//                        .disable()
-//                )
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers(PathRequest.toH2Console()).permitAll()
-//                )
-//                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
-//        return http.build();
-//    }
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
-        http.headers(headers -> headers
-                .frameOptions(frameOptions -> frameOptions.disable())
-        );
-//                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
-        http.authorizeHttpRequests(authorize -> authorize
-//                   .requestMatchers(PathRequest.toH2Console()).permitAll()
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/**")).permitAll()
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/users/**")).permitAll()
-                        .requestMatchers(PathRequest.toH2Console()).permitAll()
-        );
+        http.csrf(csrf -> csrf.disable())
+                .headers(headers -> headers
+                        .frameOptions(frameOptions -> frameOptions.disable())
+                )
+                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers(AntPathRequestMatcher.antMatcher("/users/**")).permitAll()
+                                .requestMatchers(AntPathRequestMatcher.antMatcher("/**")).access(this::hasIpAddress)
+                                .requestMatchers(PathRequest.toH2Console()).permitAll()
+
+                );
         return http.build();
     }
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .csrf(csrf -> csrf.disable())
-//                .headers(authorize -> authorize
-//                        .frameOptions((frameOptions)-> frameOptions.disable()))
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .requestMatchers(WHITE_LIST).permitAll()
-//                        .requestMatchers(PathRequest.toH2Console()).permitAll())
-//                .getOrBuild();
-//    }
+    private AuthorizationDecision hasIpAddress(Supplier<Authentication> authentication, RequestAuthorizationContext object) {
+        return new AuthorizationDecision(IP_ADDRESS_MATCHER.matches(object.getRequest()));
+    }
+
 }
